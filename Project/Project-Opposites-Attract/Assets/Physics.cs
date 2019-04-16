f269a49bd5bad445e439a29e95f2e3ab42150b2d
Ruthenium <rutaspk@gmail.com>
2019-04-16 12:13:50 +0200

    Gravity

---
+++ Project/Project-Opposites-Attract/Assets/Physics.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour
+{
+    public Vector2 position;
+    public Vector2 velocity;
+    public Vector2 gravity;
+
+    private float previousTime;
+    private float currentTime;
+
+    private float dt;
+
+    void Awake()
+    {
+        dt = currentTime - previousTime;
+        position = transform.position;
+    }
+
+    void Update()
+    {
+        UpdatePosition(dt);
+    }
+
+    void UpdatePosition(float dt)
+    {
+        previousTime = currentTime;
+        currentTime = Time.time;
+
+        dt = currentTime - previousTime;
+
+        if (dt > 0.15f)
+        {
+            dt = 0.15f;
+        }
+
+        transform.position = new Vector2(transform.position.x + velocity.x * dt, transform.position.y + velocity.y * dt);
+        velocity = velocity + gravity * dt;
+    }
+}
