51300d817550bab788a6b7c024ce5087efeac95d
Ruthenium <rutaspk@gmail.com>
2019-04-20 15:50:57 +0200

    Camera pt1

---
+++ Project/Project-Opposites-Attract/Assets/CameraForTwo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraForTwo : MonoBehaviour
+{
+    public List<Transform> targets;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    { 
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPos = centerPoint + offset;
+        transform.position = newPos;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+}
