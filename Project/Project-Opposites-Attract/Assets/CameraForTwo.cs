9b7f3d781f139cacc7e9fb04af4d037b8c27f45d
Ruthenium <rutaspk@gmail.com>
2019-04-21 17:33:04 +0200

    Tile map

--- Project/Project-Opposites-Attract/Assets/CameraForTwo.cs
+++ Project/Project-Opposites-Attract/Assets/CameraForTwo.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class CameraForTwo : MonoBehaviour
 {
+    private Camera mainCamera;
+
     public List<Transform> targets;
     public Vector3 offset;
+    private Vector3 velocity;
+    public float time = 0.5f;
+
+    public float minZoom = 100f;
+    public float maxZoom = 40f;
 
     private void LateUpdate()
-    { 
+    {
+        mainCamera = GetComponent<Camera>();
+        MoveCamera();
+        ZoomCamera();
+    }
+
+    void ZoomCamera()
+    {
+        float zoom = Mathf.Lerp(maxZoom, minZoom, GetDistance() / 50);
+        mainCamera.fieldOfView = zoom;
+    }
+
+    void MoveCamera()
+    {
         Vector3 centerPoint = GetCenterPoint();
         Vector3 newPos = centerPoint + offset;
-        transform.position = newPos;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, time);
+    }
+
+    float GetDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.size.x;
     }
 
     Vector3 GetCenterPoint()
