6870d116d5a1c0b061671701ecc6530ff4dbb400
Ruthenium <rutaspk@gmail.com>
2019-04-25 12:15:35 +0200

    Create/Destroy tiles

---
+++ Project/Project-Opposites-Attract/Assets/Scripts/CreateTile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class CreateTile : MonoBehaviour
+{
+    public GameObject tilemapGameObject;
+    public Tile tile;
+
+    Tilemap tilemap;
+
+    void Start()
+    {
+        tilemap = tilemapGameObject.GetComponent<Tilemap>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            CreateTiles();
+        }
+    }
+
+    void CreateTiles()
+    {
+        Vector3 hitPosition = Vector3.zero;
+
+        hitPosition.x = gameObject.transform.position.x - 0.01f * gameObject.transform.position.x + 2;
+        hitPosition.y = gameObject.transform.position.y - 0.01f * gameObject.transform.position.y;
+
+        tilemap.SetTile(tilemap.WorldToCell(hitPosition), tile);
+
+
+    }
+}
---
+++ Project/Project-Opposites-Attract/Assets/Scripts/DestroyTile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DestroyTile : MonoBehaviour
+{
+    public GameObject tilemapGameObject;
+
+    Tilemap tilemap;
+
+    void Start()
+    {
+        tilemap = tilemapGameObject.GetComponent<Tilemap>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            DestroyTiles(collision);
+        }
+    }
+
+    void DestroyTiles(Collision2D collision)
+    {
+        Vector3 hitPosition = Vector3.zero;
+        if (tilemap != null && tilemapGameObject == collision.gameObject)
+        {
+            foreach (ContactPoint2D hit in collision.contacts)
+            {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+            }
+        }
+    }
+
+}
