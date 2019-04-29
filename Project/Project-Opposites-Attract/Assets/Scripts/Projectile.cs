e1282e284f872fda676b9d407a56ad996159d07f
Ruthenium <rutaspk@gmail.com>
2019-04-29 21:25:10 +0200

    stufff

---
+++ Project/Project-Opposites-Attract/Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject projectile;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void CreateProjectile(float speed)
+    {
+        Instantiate(projectile, new Vector2 (speed, 0f), transform.rotation);
+    }
+}
