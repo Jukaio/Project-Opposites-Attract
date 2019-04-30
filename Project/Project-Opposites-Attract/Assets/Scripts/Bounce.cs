08d86dd91ad3f4d1cc8c5a619cabc09ace8b9279
Ruthenium <rutaspk@gmail.com>
2019-04-30 20:36:57 +0200

    Create projectile

---
+++ Project/Project-Opposites-Attract/Assets/Scripts/Bounce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float bounce;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("redPlayer") || collision.gameObject.CompareTag("bluePlayer"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = transform.up * bounce;
+        }
+    }
+}
--- Project/Project-Opposites-Attract/Assets/Scripts/CreateTile.cs
+++ Project/Project-Opposites-Attract/Assets/Scripts/CreateTile.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class CreateTile : MonoBehaviour
 {
-    public Tilemap tilemap;
+    Tilemap tilemap;
     public Tile tile;
 
     private Vector3 tilePos;
     public Vector2 position;
 
-    void Update()
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.I))
-        {
-            CreateTiles(position);
-        }
+        tilemap = GameObject.FindGameObjectWithTag("breakable").GetComponent<Tilemap>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("colllide");
+        CreateTiles(position);
     }
 
     void CreateTiles(Vector2 position)
     {
         tilePos.x = gameObject.transform.position.x - 0.01f * gameObject.transform.position.x + position.x;
         tilePos.y = gameObject.transform.position.y - 0.01f * gameObject.transform.position.y + position.y;
-        tilemap.SetTile(tilemap.WorldToCell(tilePos), tile);
+        if (tilemap.GetTile(tilemap.WorldToCell(tilePos)) == null)
+        {
+            tilemap.SetTile(tilemap.WorldToCell(tilePos), tile);
+        }
     }
 }
