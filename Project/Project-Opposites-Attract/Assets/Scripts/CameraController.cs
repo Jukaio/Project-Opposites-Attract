262a7b7f78529c348f11936276c9bd496652a63d
Ruthenium <rutaspk@gmail.com>
2019-05-08 21:00:14 +0200

    Camera pt1

---
+++ Project/Project-Opposites-Attract/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject mid;
+    public GameObject obj1;
+    public GameObject obj2;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    private void FixedUpdate()
+    {
+        if (obj1.transform.position.x < obj2.transform.position.x)
+        {
+
+        }
+        mid.transform.position = new Vector3((obj2.transform.position.x - obj1.transform.position.x / 2) + obj1.transform.position.x,
+            (obj2.transform.position.y - obj1.transform.position.y) + obj1.transform.position.y,
+            (obj2.transform.position.z - obj1.transform.position.z) + obj1.transform.position.z);
+
+        float posX = Mathf.SmoothDamp(transform.position.x, mid.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, mid.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+    }        
+}
