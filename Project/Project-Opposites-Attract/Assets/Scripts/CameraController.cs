ba237abbf4f20c915f4a3bd0e06bfa843e1238c2
David <David.Naussed.5395@student.uu.se>
2019-05-09 20:22:49 +0200

    Auto stash before merge of "master" and "origin/master"

--- Project/Project-Opposites-Attract/Assets/Scripts/CameraController.cs
+++ Project/Project-Opposites-Attract/Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
@@ -16,11 +17,39 @@ public class CameraController : MonoBehaviour
     public Vector3 minCameraPos;
     public Vector3 maxCameraPos;
 
-    private void FixedUpdate()
-    {
-        mid.transform.position = new Vector3((obj1.transform.position.x + obj2.transform.position.x) / 2,
-            (obj1.transform.position.y + obj2.transform.position.y) / 2, 0f);
+    public Camera leftCamera;
+    Vector3 leftCameraPos;
 
+    public Camera rightCamera;
+    Vector3 rightCameraPos;
+
+    float distanceOfPlayersHor;
+    float distanceOfPlayersVert;
+    float secondCamerasHorHalf;
+
+    public Camera mainCamera;
+
+    public Image image;
+
+    public float a;
+    public float b;
+    public float c;
+    float angleOtherSide;
+
+    private void Start()
+    {
+        mainCamera = mainCamera.GetComponent<Camera>();
+        distanceOfPlayersHor = ((mainCamera.transform.position.z * Mathf.Tan((mainCamera.fieldOfView / 2) * Mathf.PI / 180) * -1) * mainCamera.aspect) * 2;
+        distanceOfPlayersVert = (mainCamera.transform.position.z * Mathf.Tan((mainCamera.fieldOfView / 2) * Mathf.PI / 180) * -1);
+
+        secondCamerasHorHalf = 0.5f * ((rightCamera.transform.position.z * Mathf.Tan((rightCamera.fieldOfView / 2) * Mathf.PI / 180) * -1) * rightCamera.aspect) *2;
+
+        leftCameraPos = leftCamera.transform.position;
+        rightCameraPos = rightCamera.transform.position;
+    }
+
+    private void Update()
+    {
         float posX = Mathf.SmoothDamp(transform.position.x, mid.transform.position.x, ref velocity.x, smoothTimeX);
         float posY = Mathf.SmoothDamp(transform.position.y, mid.transform.position.y, ref velocity.y, smoothTimeY);
 
@@ -29,5 +58,47 @@ public class CameraController : MonoBehaviour
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
                 Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
                 Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
-    }        
+
+        leftCamera.transform.position = new Vector3(Mathf.Clamp(leftCamera.transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(leftCamera.transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(leftCamera.transform.position.z, minCameraPos.z, maxCameraPos.z));
+
+
+        Debug.Log(secondCamerasHorHalf + " second!!");
+
+        a = 0;
+        b = obj2.transform.position.y - obj1.transform.position.y;
+        c = obj2.transform.position.x - obj1.transform.position.x;
+
+        image.transform.rotation = Quaternion.Euler(0, 0, 90 + (Mathf.Rad2Deg * Mathf.Atan(b / c)));
+
+        if ((Mathf.Abs(obj1.transform.position.x - obj2.transform.position.x) > distanceOfPlayersHor - secondCamerasHorHalf * 2))
+        {
+            leftCamera.gameObject.SetActive(true);
+            rightCamera.gameObject.SetActive(true);
+            Debug.LogError("HELP!");
+            image.gameObject.SetActive(true);
+
+
+            if (obj1.transform.position.x < obj2.transform.position.x)
+            {
+                leftCamera.transform.position = new Vector3(obj1.transform.position.x, leftCameraPos.y, leftCameraPos.z);
+                rightCamera.transform.position = new Vector3(obj2.transform.position.x, rightCameraPos.y, rightCameraPos.z);
+            }
+            else
+            {
+                leftCamera.transform.position = new Vector3(obj2.transform.position.x, leftCameraPos.y, leftCameraPos.z);
+                rightCamera.transform.position = new Vector3(obj1.transform.position.x, rightCameraPos.y, rightCameraPos.z);
+            }
+ 
+        }
+        else
+        {
+            leftCamera.gameObject.SetActive(false);
+            rightCamera.gameObject.SetActive(false);
+            image.gameObject.SetActive(false);
+        }
+        Debug.Log((mainCamera.transform.position.z * Mathf.Tan((mainCamera.fieldOfView / 2) * Mathf.PI / 180) * -1)* mainCamera.aspect);
+        Debug.Log(distanceOfPlayersHor);
+    }
 }
