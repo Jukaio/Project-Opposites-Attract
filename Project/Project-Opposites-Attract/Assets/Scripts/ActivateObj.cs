a9768375ec2713a716a278e5e1c52d1f992d1d4f
Ruthenium <rutaspk@gmail.com>
2019-05-10 15:29:03 +0200

    Pressure plate

---
+++ Project/Project-Opposites-Attract/Assets/Scripts/ActivateObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObj : MonoBehaviour
+{
+    public GameObject obj;
+    private bool active;
+
+    void Start()
+    {
+        active = false; 
+        obj.SetActive(active);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        active = !active;
+        obj.SetActive(active);
+    }
+
+}
