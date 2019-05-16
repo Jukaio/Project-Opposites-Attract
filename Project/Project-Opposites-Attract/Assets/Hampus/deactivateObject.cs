efd0416eaab37d0a1de624eb0f2c4dd996d8b2e3
David <David.Naussed.5395@student.uu.se>
2019-05-17 00:35:06 +0200

    Hampus level

---
+++ Project/Project-Opposites-Attract/Assets/Hampus/deactivateObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deactivateObject : MonoBehaviour
+{
+    public GameObject obj;
+    private bool inactive;
+
+    void Start()
+    {
+        inactive = false;
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        inactive = !inactive;
+        obj.SetActive(inactive);
+    }
+
+}
